423ebf926c54edfc3a34fcd3edad7ef7c66cb8fa
Shubham Choudhary <200973876+ch0udharyji@users.noreply.github.com>
2025-11-12 12:16:14 +0000

    Refactor type definitions (AF-726)

--- src/app/get-started/layout.tsx
+++ src/app/get-started/layout.tsx
@@ -20,4 +20,4 @@ export default async function GetStartedLayout({ children }: { children: React.R
   return <>{children}</>
 }
 
-// [dev-log-sync]: 7ee87cea4bedfdf0
+// [dev-log-sync]: 4743cf6fdfff37ef
